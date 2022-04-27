7dd413d62797416a276e90486a3431e0eef2e7dd
Shuntian Li <shuntianli@Shuntians-MacBook-Air.local>
2022-04-26 21:43:38 -0600

    64: Rendering list of items using map()

--- src/components/Expense.jsx
+++ src/components/Expense.jsx
@@ -21,10 +21,16 @@ const Expense = (props) => {
        
         <Card className="expenses">
           <ExpensesFilter selected={filteredYear} onExpensesFilterChange={ExpensesChangeHandle}/>
-          <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
+          { // Rendering list of items
+          props.items.map(
+            (expense)=>{
+              return <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date}></ExpenseItem>
+           })
+          }
+          {/* <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
           <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} ></ExpenseItem>
           <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}></ExpenseItem>
-          <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date}></ExpenseItem>
+          <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date}></ExpenseItem> */}
         </Card>
      
 
