941771c623adf8e157aced826f5b3b71ad60cd00
Shuntian Li <shuntianli@Shuntians-MacBook-Air.local>
2022-04-27 23:59:31 -0600

    67: Outputing contional content

--- src/components/Expense.jsx
+++ src/components/Expense.jsx
@@ -4,7 +4,7 @@ import Card from "./Card";
 
 import ExpensesFilter from "./ExpensesFilter";
 
-import {useState} from "react";
+import { useState } from "react";
 
 const Expense = (props) => {
 
@@ -15,35 +15,37 @@ const Expense = (props) => {
     setFilteredYear(enteredYear);
   }
 
-  const filterExpenses = props.items.filter((expense)=>{
-    return expense.date.getFullYear().toString() === filteredYear  
+  const filterExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear
   })
 
   // Passing data from parent to children using props
-    return (
-     
-       
-        <Card className="expenses">
-          <ExpensesFilter selected={filteredYear} onExpensesFilterChange={ExpensesChangeHandle}/>
-          { // Rendering list of items
-          filterExpenses.map(
-            (expense,index)=>{
-              return <ExpenseItem 
-              key= {expense.id}
-              title={expense.title}  
-              amount={expense.amount} 
-              date={expense.date} />
+  return (
 
-           })
-          }
-          {/* <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
+
+    <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onExpensesFilterChange={ExpensesChangeHandle} />
+      {filterExpenses.length === 0 ? (
+        <p>No Data Found</p>
+      ) : filterExpenses.map(
+        (expense, index) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date} />
+        ))
+      }
+
+
+      {/* <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
           <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} ></ExpenseItem>
           <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}></ExpenseItem>
           <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date}></ExpenseItem> */}
-        </Card>
-     
+    </Card>
 
-      );
+
+  );
 
 }
 
